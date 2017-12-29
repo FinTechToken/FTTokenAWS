3c44d72d5e2f978399a9235c3719548ac75c993a
FinTechToken <support@tuitioncoin.com>
2017-12-28 23:49:02 -0500

    TestNet Node and nginx config (#6)

---
+++ TestChain/scripts/init_priv_chain.bat
@@ -0,0 +1,1 @@
+geth --networkid 913945103463586943 --datadir=../data --identity "@TestNet" --port 30303 --rpcport 8000 init ../config/CustomGenesis.json
---
+++ TestChain/scripts/start_chain_daemon.sh
@@ -0,0 +1,1 @@
+geth --datadir="../data" --port 30304 --gasprice 0 --targetgaslimit 6718904 --cache 2048 --nat none --networkid 913945103463586943 console
