b0d0dcf3b44a9c2d751b5d6d82fbca3439347ec0
Unknown <support@tuitioncoin.com>
2018-05-06 13:49:32 -0400

    Byzantium and alloc 1T to new chain
    Minors get 0. 1T will be held by contract.

--- TestChain/scripts/start_chain_daemon.sh
+++ TestChain/scripts/start_chain_daemon.sh
@@ -1,1 +1,1 @@
-geth --datadir="../data" --port 30304 --gasprice 0 --targetgaslimit 6718904 --cache 2048 --nat none --networkid 913945103463586943 console
+geth --datadir="../data" --port 30304 --gasprice 0 --targetgaslimit 10485760 --cache 2048 --nat none --networkid 913945103463586943 console
