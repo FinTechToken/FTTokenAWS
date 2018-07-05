2b50f88784fb7a5a0c6610234f13a14440ec01b6
FinTechToken <support@tuitioncoin.com>
2018-07-05 01:15:26 -0400

    hashSend added refer a friend ability (#23)

--- Lambda/hashSend/hashSend.js
+++ Lambda/hashSend/hashSend.js
@@ -2,9 +2,10 @@
 /*
   { account: PublicEthereumAddress, token: FTToken_Token } // Responds With Any Unclaimed Sent Hashes AND Hashes Received { sent:[], received:[] }
   { account: PublicEthereumAddress, token: FTToken_Token, phone: PhoneToSendHashTo } // Creates and Responds with Hash
-  { master: PublicEthereumAddress, token: FTToken_Token, delete: Hash } //Hash that was fulfilled.
+  { account: PublicEthereumAddress, token: FTToken_Token, phone: PhoneToSendHashTo, refer: true } // Creates and Responds with Hash IF unique phone
+  { master: PublicEthereumAddress, token: FTToken_Token, delete: Hash } //Hash that was fulfilled. (respond with referee:newAccount,referer:senderAccount) OR respond "Deleted"
   { master: PublicEthereumAddress, token: FTToken_Token, funded: Hash } //text phone and update Hash
-
+  { master: PublicEthereumAddress, token: FTToken_Token, funded: Hash, refer: true } //text phone with invite message and update Hash
 */
 var AWS = require('aws-sdk'),
   uuid = require('uuid/v4'),
@@ -26,12 +27,18 @@ var AWS = require('aws-sdk'),
   request,
   phone,
   respondToRequest,
+  sender,
+  referee,
   response = {sent:[], received:[]};
 
 exports.handler = (event, context, callback) => {
+    sender = null;
+    referee = null;
+    phone=null;
+    newHash = null;
+    response = {sent:[], received:[]};
     respondToRequest = callback;
     request = event;
-    
     now = new Date();
     twentyMinutesAgo = new Date();
     twentyMinutesAgo.setMinutes(now.getMinutes() - 20);
@@ -42,7 +49,7 @@ exports.handler = (event, context, callback) => {
     else if(isValidMasterRequest())
       documentClient.get(searchForHash(), textPhoneAndUpdateHashORDeleteHash);
     else
-      callback('Not Proper Format', null);
+      respondToRequest('Not Proper Format', null);
   }
 
   function isValidRequest() {
@@ -65,18 +72,20 @@ exports.handler = (event, context, callback) => {
 
 function findAndRespondWithHash(err, data) {
   if(err) 
-    respondToRequest('DB Error1', null);
+    respondToRequest('DB Error', null);
   else if(isValidToken(data.Item)) {
     if(request.phone) {
-      if(isValidPhone(request.phone))
-        createAndRespondWithNewHash(data.Item[process.env.KEY_NAME]);
+      if(isValidPhone(request.phone)) {
+        sender = data.Item[process.env.KEY_NAME];
+        documentClient.query(doesPhoneExist(request.phone), createAndRespondWithNewHashIfNoPhone);
+      }
       else
-        callback('Not Proper Format', null);
+        respondToRequest('Not Proper Format', null);
     } else {
       documentClient.query(searchForSentHash(), getSentHashAndGetPhoneAndRespondWithHash);
     }
   } else 
-    respondToRequest('DB Error2', null);
+    respondToRequest('DB Error', null);
 }
 
   function isValidToken(item) {
@@ -95,27 +104,94 @@ function findAndRespondWithHash(err, data) {
       return false;
     }
 
-  function createAndRespondWithNewHash(Sender) {
-    documentClient.put(putNewHash(Sender), respondWithNewHash);
-  }
-
-    function putNewHash(Sender) {
-      newHash = keccak256(newUUID);
+    function doesPhoneExist(phone) {
       return {
-        TableName: process.env.TABLE_NAME_HASH,
-        Item: {
-          [process.env.KEY_NAME_HASH]: newHash,
-          [process.env.KEY_NAME_HASH_SENDER]: encrypt(Sender),
-          [process.env.KEY_NAME_HASH_PHONE]: encrypt(request.phone),
-          HashCreateDate: now.toISOString(),
-          UUID: encrypt(newUUID)
+        TableName: process.env.TABLE_NAME_PHONE,
+        KeyConditionExpression: process.env.KEY_NAME_PHONE +' = :a',
+        ExpressionAttributeValues: {
+          ':a': encryptOld(phone)
         }
       };
     }
 
+  function createAndRespondWithNewHashIfNoPhone(err, data) {
+    if(err)
+      respondToRequest('DB Error', null);
+    else {
+      if(data.Count) {
+        if(request.refer)
+          respondToRequest('DB Error', null); //User exists
+        else
+          createAndRespondWithNewHash(false);
+      }
+      else
+        documentClient.query(doesPhoneExistInHash(), createAndRespondWithNewHashIfNoPhoneHash);
+    }
+  }
+
+    function doesPhoneExistInHash() {
+      return {
+        TableName: process.env.TABLE_NAME_HASH,
+        IndexName: process.env.KEY_NAME_HASH_PHONE + '-index',
+        KeyConditionExpression: process.env.KEY_NAME_HASH_PHONE + ' = :a',
+        FilterExpression: "Refer=:b",
+        ExpressionAttributeValues: {
+          ':a': encrypt(request.phone),
+          ':b': true
+        }
+      };
+    }
+
+    function createAndRespondWithNewHashIfNoPhoneHash(err, data) {
+      if(err)
+        respondToRequest('DB Error', null);
+      else {
+        if(data.Count) {
+          if(request.refer)
+            respondToRequest('DB Error', null); //User exists
+          else
+            createAndRespondWithNewHash(false);
+        }
+        else
+          createAndRespondWithNewHash(true);
+      }
+    }
+
+  function createAndRespondWithNewHash(refer) {
+    documentClient.put(putNewHash(refer), respondWithNewHash);
+  }
+
+    function putNewHash(refer) {
+      newHash = keccak256(newUUID);
+      if(refer)
+        return {
+          TableName: process.env.TABLE_NAME_HASH,
+          Item: {
+            [process.env.KEY_NAME_HASH]: newHash,
+            [process.env.KEY_NAME_HASH_SENDER]: encrypt(sender),
+            [process.env.KEY_NAME_HASH_PHONE]: encrypt(request.phone),
+            HashCreateDate: now.toISOString(),
+            UUID: encrypt(newUUID),
+            Refer: true
+          }
+        };
+      else {
+        return {
+          TableName: process.env.TABLE_NAME_HASH,
+          Item: {
+            [process.env.KEY_NAME_HASH]: newHash,
+            [process.env.KEY_NAME_HASH_SENDER]: encrypt(sender),
+            [process.env.KEY_NAME_HASH_PHONE]: encrypt(request.phone),
+            HashCreateDate: now.toISOString(),
+            UUID: encrypt(newUUID)
+          }
+        };
+      }
+    }
+
     function respondWithNewHash(err, data) {
       if(err)
-        respondToRequest('DB Error3', null);
+        respondToRequest('DB Error', null);
       else
         respondToRequest(null, newHash);
     }
@@ -133,7 +209,7 @@ function findAndRespondWithHash(err, data) {
 
   function getSentHashAndGetPhoneAndRespondWithHash(err, data) {
     if(err)
-      respondToRequest('DB Error4', null);
+      respondToRequest('DB Error', null);
     else {
       if(data.Count) {
         data.Items.forEach( (item, index) => {
@@ -153,12 +229,12 @@ function findAndRespondWithHash(err, data) {
 
     function getPhoneAndRespondWithHash(err, data) {
       if(err)
-        respondToRequest('DB Error5', null);
+        respondToRequest('DB Error', null);
       else {
         if(data.Item[process.env.KEY_NAME_PHONE]) {
           documentClient.query(searchForGotHash(decryptOld(data.Item[process.env.KEY_NAME_PHONE])), respondWithHash);
         } else
-        respondToRequest('DB Error6', null);
+        respondToRequest('DB Error', null);
       }
     }
 
@@ -177,7 +253,7 @@ function findAndRespondWithHash(err, data) {
 
       function respondWithHash(err, data) {
         if(err)
-          respondToRequest('DB Error7', null);
+          respondToRequest('DB Error', null);
         else {
           if(data.Count) {
             data.Items.forEach( (item, index) => {
@@ -198,21 +274,50 @@ function searchForHash() {
 
   function textPhoneAndUpdateHashORDeleteHash(err, data) {
     if(err)
-      respondToRequest('DB Error8', null);
+      respondToRequest('DB Error', null);
     else if(data.Item) {
       if(request.funded) {
         phone = decrypt(data.Item[process.env.KEY_NAME_HASH_PHONE]);
+        request.refer=data.Item.Refer;
         if(data.Item.Texted)
           respondToRequest('Already Funded', null);
         else
           documentClient.update(updateHashWithText(), sendTextAndRespondWithSuccess);
       } else if (request.delete) {
-        documentClient.delete(searchForHash(), respondWithSuccess);
+        documentClient.get(searchForHash(), respondWithSuccessOrReferInfo);
       } else
-        respondToRequest('DB Error9', null);
+        respondToRequest('DB Error', null);
     } else
       respondToRequest('No Hash Found', null);
   }
+
+    function respondWithSuccessOrReferInfo(err, data) {
+      if(err)
+        respondToRequest('DB Error', null);
+      else if(data.Item.Refer) {
+        sender=decrypt(data.Item.Sender);
+        documentClient.query(doesPhoneExist(decrypt(data.Item.Phone)), deleteAndRespondWithReferInfo);
+      } else
+        documentClient.delete(searchForHash(), respondWithSuccess);
+    }
+
+    function deleteAndRespondWithReferInfo(err, data) {
+      if(err)
+        respondToRequest('DB Error', null);
+      else if(data.Count) {
+        referee = data.Items[0][process.env.KEY_NAME];
+        documentClient.delete(searchForHash(), respondWithReferInfo);
+      }
+      else
+        respondToRequest('DB Error', null);
+    }
+
+    function respondWithReferInfo(err, data) {
+      if(err)
+        respondToRequest(err, null);
+      else
+        respondToRequest(null, "referer:" + sender + ":referee:" + referee);
+    }
 
     function respondWithSuccess(err, data) {
       if(err)
@@ -237,18 +342,21 @@ function searchForHash() {
 
     function sendTextAndRespondWithSuccess(err, data) {
       if(err)
-        respondToRequest('DB Error11', null);
+        respondToRequest('DB Error', null);
       else {
         sendText();
       }
     }
 
     function sendText() {
+      let msg='A friend sent you tokens. Get them at FinTechToken.com.';
+      if(request.refer)
+        msg='A friend invited you to FinTechToken.com. Take one minute to create an account, so you both get $5.'
       client.messages
       .create({
         to: '+1' + phone,
         from: process.env.TWILIO_FROM,
-        body: 'A friend sent you tokens. Get them at FinTechToken.com.'
+        body: msg
       })
       .then(message => {respondToRequest(null, 'Sent_Code')});
      respondToRequest(null, 'Sent_Code');
@@ -260,6 +368,10 @@ function encrypt(text) {
 
 function decrypt(text) {
   return crypto.createDecipher(cryptAlgorithm,password).update(text,'hex','utf8');
+}
+
+function encryptOld(text) {
+  return crypto.createCipher(cryptAlgorithm,passwordOld).update(text,'utf8','hex');
 }
 
 function decryptOld(text) {
